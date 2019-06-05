4c8282143b6d8d223fa68755b43f0f7fa7643c12
Mathew Hall <mhall.js@gmail.com>
2019-06-05 11:47:04 +0100

    Test array of object(s) added

--- index.js
+++ index.js
@@ -2,21 +2,38 @@ var express = require('express');
 var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
 
+
+
+let testNumber = Math.random();
+
+const TestObject = {
+  hello: () =>  'Hello world!',
+  test: () =>  'This is a test',
+  egg: () => testNumber,
+}
+
+
+
+var root = {
+  hello: () => 'Hello world!',
+  test: () => 'This is a test',
+  egg: () => testNumber,
+  testArray: () => [TestObject]
+};
+
 var schema = buildSchema(`
   type Query {
+    hello: String,
+    test: String,
+    egg: Float,
+    testArray: [TestObject]
+  },
+  type TestObject {
     hello: String,
     test: String,
     egg: Float
   }
 `);
-
-let testNumber = Math.random();
-
-var root = {
-  hello: () => 'Hello world!',
-  test: () => 'This is a test',
-  egg: () => testNumber
-};
 
 var app = express();
 app.use(
